e3d2d13abb0bdb3cb80acd4b08670c301c029bc4
Jerry Ron Sunny <rabbitron295@gmail.com>
2022-10-25 23:18:38 +0530

    Something

--- ar.c
+++ ar.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 int main(){
-int ar[10][10];
-for(int i=0;i<9;i++){
-	for(int j=0;j<9;j++){
-		ar[i][j]=69;
+	int ar[10][10];
+	int i,j;
+	char temp;
+	for(int i=0;i<10;i++){
+		        for(int j=0;j<10;j++){
+				                ar[i][j]=69;
+						        }
 	}
-}
-for(int i=0;i<9;i++){
-	for(int j=0;j<9;j++){
-	printf("%d ",ar[i][j]);
+
+	for(i=1;i<9;i++)
+	{
+		for(j=1;j<9;j++)
+		{
+			        scanf("%c",&temp);
+				        ar[i][j]=temp;
+
+
+		}
 	}
-	printf("\n");
-}
-}
+	for(int i=1;i<9;i++){
+		        for(int j=1;j<9;j++){
+				        printf("%c ",ar[i][j]);
+					        }
+			        printf("\n");
+
+	}
+	//printf("%c ",temp);
+	}
